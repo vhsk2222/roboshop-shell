c3f34c677120765223cdca478198004f3c7046c4
vhsk222 <vhsandeepkumar@gmail.com>
2024-09-12 11:51:26 +0530

    robo shop shell scripts

---
+++ mongodb.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Sscript started executing $TIMESTAMP" &>> LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ] 
+    then 
+        echo -e "$2 ...$R Failed ..$N"
+    else 
+        echo -e "$2 ...$G SUCCESSSSS ..$N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then echo -e "$R ERROR: NOT A ROOT USER $N"
+exit 1 #You can give other than zero, that will stop the programm if the pervious commans is a failure
+else echo "You are ROOT"
+fi 
+
+cp mongo.repo /etc/yum.repos.d &>> $LOGFILE
+
+VALIDATE $? "Copied MongoDB REPO"
